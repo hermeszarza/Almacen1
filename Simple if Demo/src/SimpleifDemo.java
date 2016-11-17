d965f6bfb27a25f76c9da886d353255141126146
hermeszarza <hermeszarza@gmail.com>
2016-11-17 12:38:31 +0100

    Update SimpleifDemo.java

--- Simple if Demo/src/SimpleifDemo.java
+++ Simple if Demo/src/SimpleifDemo.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 public class SimpleifDemo {
 
 	public static void main(String[] args) {
+		\\Modificacion desde el remoto
 		Scanner input = new Scanner(System.in);
 		System.out.print("Inserta un entero: ");
 		int numero = input.nextInt();
 		
-		// Si es múltiplo de 5 muestra HiFive
+		// Si es mÃºltiplo de 5 muestra HiFive
 		if (numero % 5 == 0) {
 			System.out.println("HiFive");
 		}
 		
-		// Si es múltiplo de 2 muestra HiEven
+		// Si es mÃºltiplo de 2 muestra HiEven
 		if (numero % 2 == 0) {
 			System.out.println("HiEven");
 		}
